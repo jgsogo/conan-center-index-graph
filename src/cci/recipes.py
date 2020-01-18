52a9d99d306b912e7bf32d2c83138c03b1c603d4
jgsogo <jgsogo@gmail.com>
2020-01-18 13:52:58 +0100

    typing the run

--- src/cci/recipes.py
+++ src/cci/recipes.py
@@ -2,11 +2,12 @@ import logging
 import os
 from typing import Optional, Iterator, List
 
+from conans.model.ref import ConanFileReference
+
 import cci.types
 from cci.recipe import Recipe
 from cci.recipe_options import explode_options_without_duplicates
 from cci.repository import Repository
-from conans.model.ref import ConanFileReference
 
 log = logging.getLogger(__name__)
 
@@ -28,18 +29,23 @@ def get_recipe_list(cci_repo: Repository,
 
     # Go for the draft recipes (avoid duplicates)
     if draft_folder:
-        for recipe in get_draft_recipes(draft_folder):
+        for recipe in get_draft_recipes(draft_folder, explode_options=explode_options):
             if recipe.ref.name not in recipes:
                 yield recipe
             else:
-                log.warninig(f"Duplicate recipe in drafts: {recipe.ref.name}")
+                log.warning(f"Duplicate recipe in drafts: {recipe.ref.name}")
 
 
-def get_draft_recipes(draft_folder: cci.types.PATH):
+def get_draft_recipes(draft_folder: cci.types.PATH, explode_options: bool = False):
     log.info(f"Gather draft recipes from {draft_folder}")
 
     for recipe in os.listdir(draft_folder):
         name, _ = os.path.splitext(recipe)
         ref = ConanFileReference(name=name, version='draft', user=None, channel=None)
         conanfile = os.path.join(draft_folder, recipe)
-        yield Recipe(ref=ref, conanfile=conanfile, is_draft=True)
+        recipe = Recipe(ref=ref, conanfile=conanfile, is_draft=True)
+        if explode_options:
+            for it in explode_options_without_duplicates(recipe):
+                yield it
+        else:
+            yield recipe
