44fd01c4c4bc203cb55da392d2a68fa101dc67db
jgsogo <jgsogo@gmail.com>
2020-01-18 18:36:17 +0100

    works again

--- src/cci/recipes.py
+++ src/cci/recipes.py
@@ -1,6 +1,8 @@
 import logging
 import os
-from typing import Optional, Iterator, List
+from itertools import product
+from typing import Iterator, List
+from typing import Optional
 
 from conans.model.ref import ConanFileReference
 
@@ -39,3 +41,69 @@ def get_draft_recipes(draft_folder: cci.types.PATH):
         conanfile = os.path.join(draft_folder, recipe)
         recipe = Recipe(ref=ref, conanfile=conanfile, is_draft=True)
         yield recipe
+
+
+def _normalize_as_dict(options_):
+    if isinstance(options_, dict):
+        pass
+    elif isinstance(options_, (tuple, list)):
+        d = dict(it.split('=') for it in options_)
+        options_ = _normalize_as_dict(d)
+    return options_
+
+
+def explode_options(recipe, conan):
+    if recipe.options:
+        raise RuntimeError(f"Recipe {recipe.ref.name} has already options exploded")
+
+    # Populate the options with those which can add/remove dependencies
+    options, default_options = conan.inspect(recipe, ['options', 'default_options'])
+    options, default_options = _normalize_as_dict(options), _normalize_as_dict(default_options)
+    for it in ['shared', 'fPIC']:
+        options.pop(it, None)
+        default_options.pop(it, None)
+
+    # Combinations of all option values
+    if options:
+        opts_as_str = []
+        for opt, values in options.items():
+            # Try to enable as many things as possible to maximize dependencies
+            if set(values) == {True, False}:
+                if any([it in opt for it in ['disable_', 'without_', 'no_']]):
+                    opts_as_str.append(["{}=False".format(opt)])
+                elif any([it in opt for it in ['enable_', 'with_']]):
+                    opts_as_str.append(["{}=True".format(opt)])
+                else:
+                    opts_as_str.append(["{}={}".format(opt, v) for v in values])
+
+        for combination in product(*opts_as_str):
+            yield Recipe(ref=recipe.ref, conanfile=recipe.conanfile, options=tuple(combination),
+                         is_draft=recipe.is_draft, is_proxy=recipe.is_proxy)
+
+    # Use default options (always)
+    combination = ()
+    if default_options:
+        combination = tuple("{}={}".format(k, v) for k, v in default_options.items())
+    yield Recipe(ref=recipe.ref, conanfile=recipe.conanfile, options=combination,
+                 is_draft=recipe.is_draft, is_proxy=recipe.is_proxy)
+
+
+def explode_options_without_duplicates(recipe, conan):
+    recipes = explode_options(recipe, conan)
+
+    class HashMyAttr:
+        cmp_opts = None
+
+        def __init__(self, obj):
+            self.obj = obj
+            if self.obj.options:
+                self.cmp_opts = tuple(sorted(self.obj.options))
+
+        def __hash__(self):
+            return hash(self.cmp_opts)
+
+        def __eq__(self, other):
+            return self.cmp_opts == other.cmp_opts
+
+    for x in set(HashMyAttr(obj) for obj in recipes):
+        yield x.obj
--- src/cci/run_conan.py
+++ src/cci/run_conan.py
@@ -1,48 +1,91 @@
+import json
 import logging
-import sys
+import os
 from io import StringIO
 from typing import List, Optional
 
-from conans.client.command import Command, Conan
+from conans import tools
+from conans.client.command import Command
+from conans.client.conan_api import ConanAPIV1, ConanOutput
 from conans.client.tools import chdir
+from conans.model.ref import ConanFileReference
 
 from cci import types
+from cci.utils import temp_file
 
 log = logging.getLogger(__name__)
 
 
 class ConanWrapper:
-    cwd: types.PATH = None
-    stream = StringIO()
-    _instance = None
+    _cwd: types.PATH
+    _conan_user_home: types.PATH
 
-    def __new__(cls, cwd=None):
-        # Check if an instance exists:
-        if not cls._instance:
-            assert cwd, "First instance requires a working directory"
-            # Create new instance:
-            cls._instance = object.__new__(cls)
-
-            # We need to impostate stdout/err during creation
-            old_stdout, old_stderr = sys.stdout, sys.stderr
-            sys.stdout, sys.stderr = cls.stream, cls.stream
-            conan_api, cache, user_io = Conan.factory()
-            cls._instance.cmd = Command(conan_api)
-            sys.stdout, sys.stderr = old_stdout, old_stderr
-            cls._instance.cwd = cwd
-        return cls._instance
+    def __init__(self, conan_user_home: types.PATH, cwd: types.PATH):
+        self._cwd = cwd
+        self.stream = StringIO()
+        output = ConanOutput(self.stream, self.stream, color=False)
+        conan_api = ConanAPIV1(cache_folder=conan_user_home, output=output)
+        self.cmd = Command(conan_api)
 
     def run(self, command: List[str], cwd: Optional[types.PATH] = None):
+        log.debug(f"Run command '{' '.join(command)}'")
         self.stream.truncate(0)
         try:
-            with chdir(cwd or self.cwd):
-                self.cmd.run(command)
+            with chdir(cwd or self._cwd):
+                r = self.cmd.run(command)
+                return r, self.stream.getvalue()
         except Exception as e:
             # Conan execution failed
             log.error(f"Command unexpectedly failed: {e}")
-        return self.stream.getvalue()
+        return -1, str(e)
 
+    def inspect(self, recipe, attributes):
+        # Return options attribute and default_options as dictionary
+        with temp_file() as json_file:
+            cmd = ['inspect']
+            for it in attributes:
+                cmd.extend(['-a', it])
+            cmd.extend(['--json', json_file, recipe.conanfile])
+            self.run(cmd)
+            data = json.loads(tools.load(json_file))
+        return tuple([data[it] for it in attributes])
 
-def initialize_conan(cwd):
-    conan = ConanWrapper(cwd=cwd)
-    return conan
+    def export(self, recipe):
+        log.info(f"Export recipe {recipe.ref}")
+        self.run(command=["export", recipe.conanfile, f"{recipe.ref}@"])
+
+    def requiremens(self, recipe, profile):
+        options_cmd = []
+        if recipe.options:
+            for opt in recipe.options:
+                options_cmd.extend(['-o', opt])
+
+        with temp_file('conan.lock') as output_file:
+            cmd = ['graph', 'lock', '--build', '--profile', profile, '--lockfile', output_file] \
+                  + options_cmd + [f"{recipe.ref}@"]
+
+            try:
+                r, out = self.run(cmd)
+                if 'Invalid configuration' in out:
+                    return None, None, None
+                elif r != 0:
+                    log.warning(out)
+                    return None, None, None
+
+                content = json.loads(tools.load(output_file))
+                for _, node in content['graph_lock']['nodes'].items():
+                    reference, _ = node['pref'].split('#')
+                    ref = ConanFileReference.loads(reference)
+                    if ref == recipe.ref:
+                        reqs = [k.split('#', 1)[0] for k, _ in node.get('requires', {}).items()]
+                        breqs = [k.split('#', 1)[0] for k, _ in node.get('build_requires', {}).items()]
+                        pyreqs = [k.split('#', 1)[0] for k, _ in node.get('python_requires', {}).items()]
+                        return reqs, breqs, pyreqs
+
+                assert False, "Never get here!"
+            except Exception as e:
+                log.error(f"Error!!! {e}")
+                log.error(f" - recipe: {recipe.ref}")
+                log.error(f" - profile: {os.path.basename(profile)}")
+                log.error(r)
+        return None, None, None
